7c37b1cb01fea599a3f3bcac4dfa72c159b7c553
Johnny Gérard <john@jgerard.dev>
2026-02-24 23:09:27 +0100

    fix: replace fixed width with horizontal padding for button styles

--- src/styles/components.ts
+++ src/styles/components.ts
@@ -3,7 +3,7 @@ import { tw } from "~/utils/tw";
 
 export const buttonStyles = tw(
   textPreset7,
-  "flex h-12 w-46.25 items-center justify-center",
+  "inline-flex h-12 items-center justify-center px-8",
   "bg-red-500 text-navy-950 uppercase transition-colors",
   "hover:bg-navy-950 hover:text-white",
 );
