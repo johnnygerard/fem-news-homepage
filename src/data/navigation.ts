90598d3d1ef785fd695104a801312cf8925b4ff0
Johnny Gérard <john@jgerard.dev>
2026-02-25 01:00:17 +0100

    feat: implement mobile menu

---
+++ src/data/navigation.ts
@@ -0,0 +1,7 @@
+export const navigation = [
+  { label: "Home", href: "/" },
+  { label: "New", href: "/new" },
+  { label: "Popular", href: "/popular" },
+  { label: "Trending", href: "/trending" },
+  { label: "Categories", href: "/categories" },
+];
